40e2f19ff0f527b01108b94eac3b510dbfa4bde5
Smit Valture <valture16@gmail.com>
2023-07-06 00:24:09 +0530

    map marker fix

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect } from 'react';
+import L from 'leaflet';
+import iconLocation from '../assets/images/icon-location.svg'
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
@@ -18,11 +20,17 @@ const Map = ({ lat, lng }) => {
         return null;
     };
 
+    const customIcon = L.icon({
+        iconUrl: iconLocation,
+        iconSize: [40, 50], // Adjust the size based on your SVG marker dimensions
+        iconAnchor: [16, 70], // Adjust the anchor position if needed
+    });
+
     return (
         <MapContainer center={position} zoom={13} style={{ height: '100%', width: '100%', zIndex: "10" }}>
             <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
             <FlyToMap />
-            <Marker position={position}>
+            <Marker position={position} icon={customIcon}>
                 <Popup>Marker Location</Popup>
             </Marker>
         </MapContainer>
